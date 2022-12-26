6a984ad625445c3a0340cfaa3c8f4dc9578ac7fb
VitorMatioski <81940665+VITORMATIOSKI@users.noreply.github.com>
2022-12-26 16:50:37 -0300

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+
+     $('#fone').mask('(00) 0 0000-0000', {placeholder: '(xx) x xxxx-xxxx'});
+
+     $('#cpf').mask('000.000.000-00', {placeholder: '000.000.000-00'})
+
+     $('#cep').mask('00.000-000', {placeholder: '00.000-000'})
+})
